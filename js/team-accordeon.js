7e2d5336440759ef0a40a7dcc12403f868154e2f
Elena Chestikova <Chestikova.elena@gmail.com>
2021-02-03 19:48:36 +0500

    Add slider

---
+++ js/team-accordeon.js
@@ -0,0 +1,10 @@
+// const members = document.querySelectorAll(".member__link");
+
+// members.addEventListener("click", function(event){
+//   event.preventDefault();
+//   const target = event.target;
+
+//   if(target.classList.contains("member__link")){
+//     target.classList.add("member__link--active");
+//   }
+// })
