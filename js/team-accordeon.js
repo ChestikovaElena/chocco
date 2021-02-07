8539f80e5c4d331ee2a2b8e55e8bb15f7b35795a
Elena Chestikova <Chestikova.elena@gmail.com>
2021-02-07 11:43:56 +0500

    Add change in team-accordeon

--- js/team-accordeon.js
+++ js/team-accordeon.js
@@ -1,14 +1,55 @@
-$(".member__link").click(function(e) {
-  e.preventDefault();
+// $(".member__link").click(function(e) {
+//   e.preventDefault();
 
-  if(this.classList.contains("member__link--active")) {
-    this.classList.remove("member__link--active");
+//   if(this.classList.contains("member__link--active")) {
+//     this.classList.remove("member__link--active");
+//   } else {
+//     $(".member__link").each( function(index, item) {
+//       if ($(item).hasClass("member__link--active")) {
+//         this.classList.remove("member__link--active");
+//       }
+//     })
+//     this.classList.add("member__link--active");
+//   }
+// })
+
+function accordeonTeam() {
+  const windowWidth = window.innerWidth;
+  if (windowWidth > 768) {
+    const team = document.querySelector(".team__list--version--desktop");
   } else {
-    $(".member__link").each( function(index, item) {
-      if ($(item).hasClass("member__link--active")) {
-        this.classList.remove("member__link--active");
-      }
-    })
-    this.classList.add("member__link--active");
+    const team = document.querySelector(".team__list--version--mobile");
   }
-})
+
+  const members = team.querySelectorAll(".members__item");
+  
+  team.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const target = e.target.parentNode;
+
+    if(target.classList.contains("member__link")) {
+      const member = target.parentNode;
+      const content = target.nextElementSibling;
+      const contentHeight = content.firstElementChild.clientHeight;
+
+      for (const iterator of members) {
+        if(iterator !== member) {
+          iterator.classList.remove("members__item--active");
+          iterator.lastElementChild.style.height = 0;
+        }
+      }
+      
+      if(member.classList.contains("members__item--active")) {
+        member.classList.remove("members__item--active");
+        content.style.height = 0;
+      } else {
+        member.classList.add("members__item--active");
+        content.style.height = contentHeight + "px";
+      }
+
+    };
+  })
+}
+
+accordeonTeam();
