d99dc8fe573088bba6899ed6e8f3246155133423
Elena Chestikova <Chestikova.elena@gmail.com>
2021-02-09 12:36:47 +0500

    Add bars-section

---
+++ js/bars-accordeon.js
@@ -0,0 +1,60 @@
+document.querySelector('.bars__list').addEventListener('click', (e) => {
+  e.preventDefault();
+  const trigger = e.target.closest('[data-trigger]');
+  
+  if (!trigger) return;
+
+  const item = trigger.parentNode;
+  
+  if (item.classList.contains('active')) {
+
+    closeItem(item);
+
+  } else {
+
+    closeActiveItem(item);
+    openItem(item);
+  
+  }
+})
+
+function closeActiveItem(item) {
+  const list = item.parentNode;
+  const itemActive = list.querySelector('.active');
+
+  if (!!itemActive) {
+    closeItem(itemActive);
+  }
+}
+
+function openItem(item) {
+  const contentWrap = item.querySelector('[data-open]');
+  const content = contentWrap.firstElementChild;
+  const openWidth = calctWidth(item);
+
+  content.style.minWidth = `${openWidth}px`;
+  contentWrap.style.width = `${openWidth}px`;
+  item.classList.add('active');
+
+}
+
+function closeItem(item) {
+  const contentWrap = item.querySelector('[data-open]');
+
+  contentWrap.style.width = 0;
+  item.classList.remove('active');
+}
+
+function calctWidth(item) {
+  const list = item.parentNode;
+  const windowWidth = window.innerWidth;
+  const triggers = list.querySelectorAll('[data-trigger]');
+  const triggersWidth = triggers[0].clientWidth * triggers.length;
+  const isMobile = window.matchMedia('(max-width: 768px)').matches;
+
+  if (isMobile) {
+    return windowWidth - triggersWidth;
+  }
+  return 524
+
+}
