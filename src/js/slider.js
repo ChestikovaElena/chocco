1fd9ba7d2d455ef8da04d5f69c1777365844d83f
Elena Chestikova <Chestikova.elena@gmail.com>
2021-06-27 02:23:04 +0500

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,4 @@
-// (function () {
   $('.products').slick({
     nextArrow: document.querySelector('.product-slider__arrow--dir--right'),
     prevArrow: document.querySelector('.product-slider__arrow--dir--left')
   });
-// })()
